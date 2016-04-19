59e638425d065ed786ef03542b3bc8000f45d80e
student <student@cr3309i.local>
2016-04-20 04:08:47 +0800

    Final Working Release

--- SliderCalculator/ViewController.swift
+++ SliderCalculator/ViewController.swift
@@ -8,11 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textFieldPrincipal: UITextField!
+    @IBOutlet weak var textFieldNumberOfYears: UITextField!
+    @IBOutlet weak var slidderNumberOfYears: UISlider!
+    @IBOutlet weak var textFieldInterestRate: UITextField!
+    @IBOutlet weak var slidderInterestRate: UISlider!
+    
+    @IBOutlet weak var labelAmount : UILabel!
+    
+    
+    @IBAction func sliderNumberOfYearsChanghed(sender: AnyObject){
+        let valueNumberofYears : Int = Int(slidderNumberOfYears.value)
+        textFieldNumberOfYears.text = String(valueNumberofYears)
+        self.displayAmount(sender)
+    }
+    
+    @IBAction func sliderInterestRateChanghed(sender: AnyObject){
+        let valueInterestRate : Float = Float(slidderInterestRate.value)
+        textFieldInterestRate.text = NSString(format: "%.2f", valueInterestRate) as String
+        self.displayAmount(sender)
+    }
 
+
+    @IBAction func displayAmount(sender: AnyObject) {
+        let principalAmount : Int = Int(textFieldPrincipal.text!)!
+        let numberOfYears : Int = Int(textFieldNumberOfYears.text!)!
+        let interestRate : Float = NSString(string: textFieldInterestRate.text!).floatValue / 100
+        let finalAmount = Double(principalAmount) * pow(Double(1+interestRate), Double(numberOfYears))
+        labelAmount.text = NSString(format: "%.2f", finalAmount) as String
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textFieldPrincipal.text = "10000"
+        textFieldNumberOfYears.text = "10"
+        slidderNumberOfYears.value = 10
+        textFieldInterestRate.text = "1"
+        slidderInterestRate.value = 1
+        
+        textFieldPrincipal.delegate = self
+        textFieldNumberOfYears.delegate = self
+        textFieldInterestRate.delegate = self
+        
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField){
+        self.displayAmount(textField)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +66,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
